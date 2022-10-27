f3fcd598adca0209f94aca76de260c02bee64779
Tikhomirowww <you@example.com>
2022-10-27 15:09:16 +0600

    fixed

--- src/context/AuthContextProvider.js
+++ src/context/AuthContextProvider.js
@@ -5,7 +5,7 @@ import { useNavigate } from 'react-router-dom'
 export const authContext = React.createContext();
 export const useAuth = () => useContext(authContext)
 
-const API =  "http://localhost:3000/users"
+const API =  "http://localhost:8000/users"
 
 
 const AuthContextProvider = ({ children }) => {
