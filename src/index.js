5f7bf945df86c48c3cc5493eb099aecb7ef6e88e
Tikhomirowww <you@example.com>
2022-10-28 14:55:17 +0600

    search

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './style.css'
+import './components/Navbar/Navbar.css'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
